40a6f17b47fda3ad109c1379724fcf0bcefdf3bc
= <=>
2019-09-02 22:10:15 -0600

    Primer comit

---
+++ lugar/lugar.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+const getLugarLatLng = async(dir) => {
+    const encodedUlr = encodeURI(dir);
+
+    const instance = axios.create({
+        baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodedUlr}`,
+        headers: { 'X-RapidAPI-Key': 'c71012e302mshfde1497190d99fbp120d87jsn3d319a2551cc' }
+    });
+
+
+    const resp = await instance.get();
+
+    if (resp.data.Results.length === 0) {
+        throw new Error(`No hay resultados para ${dir}`);
+    }
+
+    const data = resp.data.Results[0];
+    const direccion = data.name;
+    const lat = data.lat;
+    const lng = data.lon;
+
+    return {
+        direccion,
+        lat,
+        lng
+    }
+}
+
+module.exports = {
+    getLugarLatLng
+}
